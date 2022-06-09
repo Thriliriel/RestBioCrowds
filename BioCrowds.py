1276c4d8d330f95c754d9a4bde1e1877c68f9424
Thriliriel <wsk.shadow@gmail.com>
2022-06-09 13:22:43 -0300

    for heroku.

--- BioCrowds.py
+++ BioCrowds.py
@@ -338,14 +338,14 @@ class BioCrowds():
 			return pd.DataFrame(writeResult)
 
 	def ConnectDB(self):
-		#self.conn = mysql.connector.connect(host='localhost',
-  #                                       database='biocrowds',
-  #                                       user='root',
-  #                                       password='')
-		self.conn = psycopg2.connect(host="localhost",
-								database="biocrowds",
-								user="postgres",
-								password="postgres")
+		#self.conn = psycopg2.connect(host="localhost",
+		#						database="biocrowds",
+		#						user="postgres",
+		#						password="postgres")
+		
+		#heroku
+		DATABASE_URL = os.environ.get('DATABASE_URL')
+		self.conn = psycopg2.connect(DATABASE_URL)
 
 	def ClearDatabase(self):
 		cursor = self.conn.cursor()
