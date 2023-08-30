c1133758c343d8d614ad6c773fc58d3d7d78786f
GabrielFSilva <gabrielsilva.prog@gmail.com>
2023-08-30 19:26:19 -0300

    experiments median ref agent

--- LocalExperimentCompilation.py
+++ LocalExperimentCompilation.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
     configs_per_scenario = [3, 3, 3, 4, 3]
     hypotenuse_per_scenario = [42.426406871192, 42.426406871192, 42.426406871192, 42.426406871192, 61.84658438426490824]
     seeds_per_scenario = [50, 50, 70, 40, 20]
+    #seeds_per_scenario = [27, 34, 70, 9, 10]
     config_names = ["ConfigA", "ConfigB", "ConfigC", "ConfigD"]
     base_path = Path() / "OutputExperiments"
 
